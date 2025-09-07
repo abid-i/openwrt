df98ddf567704e10d91571f2a9bd0787b7624f8a
Abid Ismail <abid.ismail@protonmail.com>
2025-09-07 11:45:41 +0530

    update-test

--- target/linux/ipq40xx/image/tp-link.mk
+++ target/linux/ipq40xx/image/tp-link.mk
@@ -8,6 +8,7 @@ define Device/tplink_deco-m4r-v3
   SOC := qcom-ipq4019
   BLOCKSIZE := 64k
   PAGESIZE := 2048
+  # Combined firmware partition approach (15.375MB total space)
   IMAGE_SIZE := 15744k
   DEVICE_DTS := qcom-ipq4019-tplink-deco-m4r-v3
   DEVICE_DTS_CONFIG := config@ap.dk04.1-c1
@@ -17,8 +18,24 @@ define Device/tplink_deco-m4r-v3
 		kmod-leds-gpio \
 		kmod-gpio-button-hotplug \
 		kmod-crypto-hw-qce \
-		hostapd-common \
-		wpad-basic-mbedtls \
+		ath10k-board-qca4019 \
+		ath10k-firmware-qca4019 \
+		kmod-ath10k \
+		kmod-batman-adv \
+		batctl-full \
+		mesh11sd \
+		hostapd-full \
+		wpad-mesh-mbedtls \
+		bridge-utils \
+		vlan \
+		kmod-8021q \
+		kmod-br-netfilter \
+		kmod-nft-offload \
+		kmod-sched-cake \
+		irqbalance \
+		kmod-tcp-bbr \
+		tc-tiny \
+		ethtool \
 		iwinfo \
 		iw-full \
 		wireless-regdb \
@@ -26,26 +43,20 @@ define Device/tplink_deco-m4r-v3
 		uci \
 		netifd \
 		firewall4 \
-		kmod-nft-offload \
-		bridge-utils \
-		kmod-sched-cake \
-		irqbalance \
-		tc-tiny \
 		luci \
 		luci-base \
 		luci-mod-admin-full \
 		luci-theme-bootstrap \
-		luci-proto-ipv6 \
 		luci-app-firewall \
+		luci-proto-batman-adv \
+		luci-app-batman-adv \
+		luci-app-advanced-reboot \
 		ip-full \
-		ipset \
 		curl \
 		wget-ssl \
 		ca-certificates \
 		openssl-util \
-		openssh-sftp-server \
-		kmod-tcp-bbr \
-		ethtool \
+		libopenssl-devcrypto \
 		-ppp-mod-pppoe \
 		-ppp \
 		-odhcp6c \
@@ -60,36 +71,37 @@ define Device/tplink_deco-m4r-v3
 		-kmod-pppoe \
 		-dnsmasq \
 		-odhcpd-ipv6only \
-		-dropbear
+		-dropbear \
+		-wpad-basic-mbedtls
 
-  # Image generation with automatic MTD splitting
+  # Image generation with combined firmware partition
   IMAGES := factory.bin sysupgrade.bin
   IMAGE/factory.bin := tplink-safeloader
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   
+  # Performance optimization compiler flags
   TARGET_CFLAGS += -O3 -ffunction-sections -fdata-sections \
-                   -fomit-frame-pointer -funroll-loops
+                   -fomit-frame-pointer -funroll-loops \
+                   -mcpu=cortex-a7 -mfpu=neon-vfpv4 \
+                   -mfloat-abi=hard
   
   TARGET_LDFLAGS += -Wl,--gc-sections -Wl,--as-needed -Wl,-O1
 
-  # # IPQ4019 specific hardware optimizations 
-  # TARGET_CPPFLAGS += -DCONFIG_IPQ4019_EDMA_OPTIMIZATION \
-  #                    -DCONFIG_BATMAN_ADV_BATMAN_V \
-  #                    -DCONFIG_ATH10K_SPECTRAL \
-  #                    -DCONFIG_ATH10K_DFS_CERTIFIED \
-  #                    -DCONFIG_MAC80211_MESH \
-  #                    -DCONFIG_HOSTAPD_WPS \
-  #                    -DCONFIG_IEEE80211R \
-  #                    -DCONFIG_IEEE80211W \
-  #                    -DCONFIG_HS20 \
-  #                    -DCONFIG_WNM \
-  #                    -DCONFIG_FST \
-  #                    -DCONFIG_MBO \
-  #                    -DCONFIG_OCV \
-  #                    -DCONFIG_SAE \
-  #                    -DCONFIG_EAP_SERVER=0 \
-  #                    -DCONFIG_RADIUS_CLIENT=1
+  # IPQ4019 specific hardware optimizations
+  TARGET_CPPFLAGS += -DCONFIG_IPQ4019_EDMA_OPTIMIZATION \
+                     -DCONFIG_BATMAN_ADV_BATMAN_V \
+                     -DCONFIG_ATH10K_SPECTRAL \
+                     -DCONFIG_ATH10K_DFS_CERTIFIED \
+                     -DCONFIG_MAC80211_MESH \
+                     -DCONFIG_HOSTAPD_WPS \
+                     -DCONFIG_IEEE80211R \
+                     -DCONFIG_IEEE80211W \
+                     -DCONFIG_HS20 \
+                     -DCONFIG_WNM \
+                     -DCONFIG_MBO \
+                     -DCONFIG_SAE
   
   SUPPORTED_DEVICES += deco-m4r-v3
 endef
 TARGET_DEVICES += tplink_deco-m4r-v3
+
