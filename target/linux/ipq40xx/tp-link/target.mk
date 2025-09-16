3cca788255c9af0a90c25d33bf3618344a4437fd
Abid Ismail <abid.ismail@protonmail.com>
2025-09-16 11:47:02 +0530

    rename

--- target/linux/ipq40xx/tp-link/target.mk
+++ target/linux/ipq40xx/tp-link/target.mk
@@ -1,4 +1,4 @@
-BOARDNAME:=TP-Link-Deco
+BOARDNAME:=TP-Link
 DEFAULT_PACKAGES += -ath10k-firmware-qca4019-ct -kmod-ath10k-ct \
   -kmod-usb-dwc3-qcom -kmod-usb3 kmod-usb-dwc3 -kmod-usb3 -kmod-usb2 \
 	-kmod-usb-ohci -kmod-usb-ehci -usb-utils
