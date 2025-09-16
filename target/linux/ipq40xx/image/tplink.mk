d03eeb7f945354f4b507168c291f3ccfdda51242
Abid Ismail <abid.ismail@protonmail.com>
2025-09-16 15:49:07 +0530

    updatw

--- target/linux/ipq40xx/image/tplink.mk
+++ target/linux/ipq40xx/image/tplink.mk
@@ -7,13 +7,12 @@ define Device/FitImage
 endef
 
 
-define Device/tplink_deco-m4r-v3
-  $(call Device/FitImage)
+define Device/tplink_deco-m4r-v3-base
   DEVICE_VENDOR := TP-Link
+  SOC := ipq4019
   DEVICE_MODEL := Deco M4R
-  DEVICE_VARIANT := v3
-  BOARD_NAME := deco-m4r-v3
-  SOC := qcom-ipq4019
+	BOARD_NAME := deco-m4r-v3
+  TPLINK_BOARD_ID := DECO-M4R-V3
   
   BLOCKSIZE := 64k
   PAGESIZE := 2048
@@ -22,10 +21,17 @@ define Device/tplink_deco-m4r-v3
   
   DEVICE_DTS := qcom-ipq4019-tplink-deco-m4r-v3
   DEVICE_DTS_CONFIG := config@ap.dk04.1-c1 
-  
-  TPLINK_BOARD_ID := DECO-M4R-V3
-  
-  # Core mesh networking packages
+
+  IMAGES := factory.bin sysupgrade.bin
+  IMAGE/factory.bin := append-rootfs | tplink-safeloader factory | check-size
+  IMAGE/sysupgrade.bin := append-rootfs | tplink-safeloader sysupgrade | append-metadata | check-size
+endef
+
+
+define Device/tplink_deco-m4r-v3
+  $(call Device/FitImage)
+	$(call Device/tplink_deco-m4r-v3-base)
+  DEVICE_VARIANT := v3
   DEVICE_PACKAGES := \
     ath10k-board-qca4019 ipq-wifi-tplink_deco-m4r-v3 ath10k-firmware-qca4019 \
     hostapd-full wpad-mesh-mbedtls iw-full iwinfo wireless-regdb batctl-default \
@@ -37,23 +43,26 @@ define Device/tplink_deco-m4r-v3
     kmod-ath10k kmod-mac80211-mesh dnsmasq-dhcpv6 kmod-mac80211 kmod-leds-gpio \
     kmod-gpio-button-hotplug kmod-dsa kmod-dsa-qca8k kmod-leds-lp55xx-common lua \
     luci-mod-dashboard
-    
-  # Remove unnecessary packages to save space and avoid conflicts
   DEVICE_PACKAGES += \
      -wpad-basic-mbedtls -ppp -ppp-mod-pppoe -kmod-pppoe \
 	  -kmod-pppox -kmod-slhc
-  
-  IMAGES := factory.bin sysupgrade.bin
-  IMAGE/factory.bin := append-rootfs | tplink-safeloader factory | check-size
-  IMAGE/sysupgrade.bin := append-rootfs | tplink-safeloader sysupgrade | append-metadata | check-size
-  
-
-	KERNEL_PATCHVER := 6.12
-
-  SUPPORTED_DEVICES += deco-m4r-v3
-
-  
 endef
 
 TARGET_DEVICES += tplink_deco-m4r-v3
+
+define Device/tplink_deco-m4r-v3-gateway
+  $(call Device/FitImage)
+	$(call Device/tplink_deco-m4r-v3-base)
+  DEVICE_VARIANT := Gateway
+  DEVICE_PACKAGES += hostapd-openssl wpad-basic-openssl mesh11sd batman-adv
+endef
+TARGET_DEVICES += tplink_deco-m4r-v3-gateway
+
+define Device/tplink_deco-m4r-v3-node
+  $(call Device/FitImage)
+  $(call Device/tplink_deco-m4r-v3)
+  DEVICE_VARIANT := Node
+  DEVICE_PACKAGES += wpad-basic-openssl mesh11sd batman-adv
+endef
+TARGET_DEVICES += tplink_deco-m4r-v3-node
 
--- target/linux/ipq40xx/tplink/target.mk
+++ target/linux/ipq40xx/tplink/target.mk
@@ -1,6 +1,23 @@
 SUBTARGET:=tplink
 BOARDNAME:=TP-Link
+FEATURES:=
+
+include $(INCLUDE_DIR)/target.mk
+
 DEFAULT_PACKAGES += -ath10k-firmware-qca4019-ct -kmod-ath10k-ct \
-  -kmod-usb-dwc3-qcom -kmod-usb3 kmod-usb-dwc3 -kmod-usb3 -kmod-usb2 \
+	-kmod-usb-dwc3-qcom -kmod-usb3 kmod-usb-dwc3 -kmod-usb3 -kmod-usb2 \
 	-kmod-usb-ohci -kmod-usb-ehci -usb-utils
+
+define Target/Description
+	Build firmware images for TP-Link devices based on IPQ40xx
+endef
+
+
+ifeq ($(CONFIG_TARGET_ipq40xx_tplink_DEVICE_tplink_deco-m4r-v3-gateway),y)
+  DEFAULT_PACKAGES += kmod-batman-adv kmod-lib-crc16
+endif
+
+ifeq ($(CONFIG_TARGET_ipq40xx_tplink_DEVICE_tplink_deco-m4r-v3-node),y)
+  DEFAULT_PACKAGES += kmod-batman-adv
+endif
 
